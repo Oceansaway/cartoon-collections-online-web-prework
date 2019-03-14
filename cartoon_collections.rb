95be827ff08fb6f3dfc0a9139110092888076cb9
Stephanie Purple <spurple133@gmail.com>
2019-03-14 15:16:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |item,index|
+  puts "#{index+1}. #{item}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(cartoons)
+  cartoons.collect do |x|
+  x.capitalize+"!"
+end
+  end
+
+def long_planeteer_calls(calls)
+  calls.any? do |x|
+    x.length > 4
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
